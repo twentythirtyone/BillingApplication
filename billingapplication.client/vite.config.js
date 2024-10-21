d2879dc7d1b0d5d550ed629c626664ee1b2a6806
holo2k <nneketaa@yandex.ru>
2024-10-21 23:00:04 +0500

    delete template files

--- billingapplication.client/vite.config.js
+++ billingapplication.client/vite.config.js
@@ -43,7 +43,7 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            '^/weatherforecast': {
+            '^/billingapplication': {
                 target,
                 secure: false
             }
