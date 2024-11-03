e2e84b8d887580d313b78476e8b5c77d15aa8a47
ArcKontyR <kontyrtime@mail.ru>
2024-11-03 17:41:56 +0500

    Extras added

---
+++ BillingApplication.Server/DataLayer/Repositories/ExtrasRepository.cs
@@ -0,0 +1,39 @@
+﻿using BillingApplication.DataLayer.Entities;
+using BillingApplication.Server.Mapper;
+using BillingApplication.Services.Models.Utilites;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillingApplication.DataLayer.Repositories
+{
+    public class ExtrasRepository : IExtrasRepository
+    {
+        private readonly BillingAppDbContext context;
+        public ExtrasRepository(BillingAppDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<int?> Create(Extras extras)
+        {
+            BundleEntity bundle = await context.Bundles.FindAsync(extras.Package) ?? throw new InvalidOperationException("bundle_does_not_exists"); 
+
+            ExtrasEntity extrasEntity = ExtrasMapper.ExtrasModelToExtrasEntity(extras);
+            await context.Extras.AddAsync(extrasEntity);
+            await context.SaveChangesAsync();
+
+            return extrasEntity?.Id;
+        }
+
+        public async Task<IEnumerable<Extras?>> Get()
+        {
+            IEnumerable<ExtrasEntity> extras = await context.Extras.ToListAsync();
+            return extras.Select(ExtrasMapper.ExtrasEntityToExtrasModel);
+        }
+
+        public async Task<Extras?> GetById(int? id)
+        {
+            ExtrasEntity? extrasEntity = await context.Extras.FindAsync(id) ?? throw new InvalidOperationException("no_extras_found");
+            Extras? extras = ExtrasMapper.ExtrasEntityToExtrasModel(extrasEntity);
+            return extras;
+        }
+    }
+}
---
+++ BillingApplication.Server/Mapper/ExtrasMapper.cs
@@ -0,0 +1,35 @@
+﻿using BillingApplication.DataLayer.Entities;
+using BillingApplication.Services.Models.Utilites;
+
+namespace BillingApplication.Server.Mapper
+{
+    public class ExtrasMapper
+    {
+        public static Extras? ExtrasEntityToExtrasModel(ExtrasEntity? extrasEntity)
+        {
+            if (extrasEntity == null)
+                return null;
+            return new Extras()
+            {
+                Id = extrasEntity.Id,
+                Description = extrasEntity.Description,
+                Package = extrasEntity.Package,
+                Price = extrasEntity.Price,
+                Title = extrasEntity.Title
+            };
+        }
+
+        public static ExtrasEntity ExtrasModelToExtrasEntity(Extras? extrasModel)
+        {
+            if (extrasModel == null)
+                return null;
+            return new ExtrasEntity()
+            {
+                Description = extrasModel.Description,
+                Package = extrasModel.Package,
+                Price = extrasModel.Price,
+                Title = extrasModel.Title
+            };
+        }
+    }
+}
