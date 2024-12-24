7742b4bec5cd8c22690513ba9a3dc8ffd675db3d
holo2k <nneketaa@yandex.ru>
2024-12-24 23:16:54 +0500

    logs gitignore, ping method add

---
+++ BillingApplication.Server/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using BillingApplication.Mapper;
+using BillingApplication.Server.Services.Manager.SubscriberManager;
+using BillingApplication.Services.Auth;
+using BillingApplication.Services.Models.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillingApplication.Server.Controllers
+{
+    [ApiController]
+    [Route("ping")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        public async Task<IActionResult> Ping()
+        {
+            return Ok();
+        }
+    }
+}
